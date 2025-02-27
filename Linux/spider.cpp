0fb2c99e86235e263bffa6f0a676739bdf3eabdf
shuichiro-endo <29345543+shuichiro-endo@users.noreply.github.com>
2025-02-27 14:00:26 +0900

    version up: 0.12

--- Linux/spider.cpp
+++ Linux/spider.cpp
@@ -1,7 +1,7 @@
 /*
  * Title:  spider spider.cpp (Linux)
  * Author: Shuichiro Endo
- * Ver:    0.11
+ * Ver:    0.12
  */
 
 #include "spider.hpp"
@@ -2357,7 +2357,7 @@ namespace spider
         std::printf("  :-.  ::+=-:--=:=*-             _//_// _// _//_/   _//_/         _//     \n");
         std::printf("         -+: ++-  -*-        _// _//_//     _// _// _//  _////   _///     \n");
         std::printf("        :*-  :*-   .:.              _//                                   \n");
-        std::printf("        =-    -:                 Linux Ver: 0.11  Author: Shuichiro Endo  \n");
+        std::printf("        =-    -:                 Linux Ver: 0.12  Author: Shuichiro Endo  \n");
         std::printf("\n");
     }
 
