0de78466e021245025968722ee59c939125339aa
shuichiro-endo <29345543+shuichiro-endo@users.noreply.github.com>
2025-03-23 09:37:01 +0900

    version up: 1.13

--- Linux_static/spider.cpp
+++ Linux_static/spider.cpp
@@ -1,7 +1,7 @@
 /*
  * Title:  spider spider.cpp (Linux static)
  * Author: Shuichiro Endo
- * Ver:    1.12
+ * Ver:    1.13
  */
 
 #include "spider.hpp"
@@ -52,7 +52,7 @@ namespace spider
         std::printf("  :-.  ::+=-:--=:=*-             _//_// _// _//_/   _//_/         _//     \n");
         std::printf("         -+: ++-  -*-        _// _//_//     _// _// _//  _////   _///     \n");
         std::printf("        :*-  :*-   .:.              _//                                   \n");
-        std::printf("        =-    -:          Linux static Ver: 1.12  Author: Shuichiro Endo  \n");
+        std::printf("        =-    -:          Linux static Ver: 1.13  Author: Shuichiro Endo  \n");
         std::printf("\n");
     }
 
