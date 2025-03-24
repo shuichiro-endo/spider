10ac03512e2bdde7a6cc73e35aeda9c6ec4584ff
shuichiro-endo <29345543+shuichiro-endo@users.noreply.github.com>
2025-03-25 05:15:01 +0900

    version up: 1.16

--- Windows/spider.cpp
+++ Windows/spider.cpp
@@ -1,7 +1,7 @@
 /*
  * Title:  spider spider.cpp (Windows)
  * Author: Shuichiro Endo
- * Ver:    1.15
+ * Ver:    1.16
  */
 
 #define NOMINMAX
@@ -53,7 +53,7 @@ namespace spider
         std::printf("  :-.  ::+=-:--=:=*-             _//_// _// _//_/   _//_/         _//     \n");
         std::printf("         -+: ++-  -*-        _// _//_//     _// _// _//  _////   _///     \n");
         std::printf("        :*-  :*-   .:.              _//                                   \n");
-        std::printf("        =-    -:               Windows Ver: 1.15  Author: Shuichiro Endo  \n");
+        std::printf("        =-    -:               Windows Ver: 1.16  Author: Shuichiro Endo  \n");
         std::printf("\n");
     }
 
